aa5a9b850bc3a803d24697bea55710c8e0d99100
ccuervo10 <42391108+ccuervo10@users.noreply.github.com>
2018-11-19 18:21:40 -0500

    correccion de los nombres de los archivos

--- hw4.mk
+++ hw4.mk
@@ -7,13 +7,13 @@ Resultados_hw4.pdf: Resultados_hw4.tex 0_0.png 1_0.png 2_0.png proyectil_x.png p
 	pdflatex Resultados_hw4.tex > log_tex
 
 0_0.png 1_0.png 2_0.png proyectil_x.png proyectil_x_varios.png: 0.txt 1.txt 2.txt proyectil_x.txt proyectil_x_varios.txt
-	python plots.py
+	python Plots_hw4.py
 
-0.txt 1.txt 2.txt: pde.cpp
-	g++ pde.cpp -o calcita_varilla.x
+0.txt 1.txt 2.txt: PDE.cpp
+	g++ PDE.cpp -o calcita_varilla.x
 	./calcita_varilla.x
 
 
-proyectil_x.txt proyectil_x_varios.txt: ode.cpp
-	g++ ode.cpp -o proyectil_con_friccion.x
+proyectil_x.txt proyectil_x_varios.txt: ODE.cpp
+	g++ ODE.cpp -o proyectil_con_friccion.x
 	./proyectil_con_friccion.x
