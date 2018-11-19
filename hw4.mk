d0af592f780f8e5947309f25451346a4dbd6b47b
ccuervo10 <42391108+ccuervo10@users.noreply.github.com>
2018-11-19 18:12:45 -0500

    archivo final

--- hw4.mk
+++ hw4.mk
@@ -1,6 +1,12 @@
-all: 0.png 1.png 2.png
+all: Resultados_hw4.pdf
 
-0.png 1.png 2.png: 0.txt 1.txt 2.txt
+
+Resultados_hw4.pdf: Resultados_hw4.tex 0_0.png 1_0.png 2_0.png proyectil_x.png proyectil_x_varios.png
+	# Compilar dos veces para que las referencias queden bien
+	pdflatex Resultados_hw4.tex > log_tex
+	pdflatex Resultados_hw4.tex > log_tex
+
+0_0.png 1_0.png 2_0.png proyectil_x.png proyectil_x_varios.png: 0.txt 1.txt 2.txt proyectil_x.txt proyectil_x_varios.txt
 	python plots.py
 
 0.txt 1.txt 2.txt: pde.cpp
@@ -8,7 +14,6 @@ all: 0.png 1.png 2.png
 	./calcita_varilla.x
 
 
-proyectil_x.txt proyectil_.txt: ode.cpp
+proyectil_x.txt proyectil_x_varios.txt: ode.cpp
 	g++ ode.cpp -o proyectil_con_friccion.x
 	./proyectil_con_friccion.x
-
